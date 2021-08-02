45f2bb1b63427a7972d4afefae3eab8a23502167
MrVikas <>
2021-08-02 10:47:05 +0530

    Day2

--- JavaScript/Section1.js
+++ JavaScript/Section1.js
@@ -2,7 +2,7 @@
 
 /****  Section 1👉 we need to do it in console ****/
 // alert("Welcome, to Complete JavaScript course");
-console.log('Welcome, to complete JavaScript Course 🔥');
+console.log('Welcome, to complete JavaScript Course  🤦‍♂️🔥');
 
 /**** Section 2👉 Code Editor for writing JS ****/
 
